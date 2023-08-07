c6fa309d47c6afbde902c751107b1a0a82769136
Stephen Dzakpasu <steve.dzakpasu@gmail.com>
2023-08-07 14:21:14 +0000

    added lecture details screen

---
+++ screens/LectureDetails.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function LectureDetails() {
+  return (
+    <View>
+      <Text>LectureDetails</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
